e728e5fc04c2bac1b10080d949e381dad7c51821
BarisKarapelit <baris_karapelit_298@hotmail.com>
2022-06-08 15:09:56 +0300

    Urun Ekleme Ekrani Ekledin

--- OnlineTicaretUygulamasi/AnaMenü/Urunler.Designer.cs
+++ OnlineTicaretUygulamasi/AnaMenü/Urunler.Designer.cs
@@ -33,16 +33,45 @@ namespace OnlineTicaretUygulamasi.AnaMenü
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.backToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUrunAdi = new System.Windows.Forms.TextBox();
+            this.txtMaliyet = new System.Windows.Forms.TextBox();
+            this.Maliyet = new System.Windows.Forms.Label();
+            this.txtYorum = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtStokMiktari = new System.Windows.Forms.TextBox();
+            this.lblStok = new System.Windows.Forms.Label();
+            this.txtOzellikler = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.lblFiyat = new System.Windows.Forms.Label();
+            this.lblKategori = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cmbKategori = new System.Windows.Forms.ComboBox();
+            this.dtUrunler = new System.Windows.Forms.DataGridView();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtUrunler)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1600, 48);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -51,30 +80,266 @@ namespace OnlineTicaretUygulamasi.AnaMenü
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.backToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(72, 40);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // backToolStripMenuItem
             // 
             this.backToolStripMenuItem.Name = "backToolStripMenuItem";
-            this.backToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.backToolStripMenuItem.Size = new System.Drawing.Size(198, 44);
             this.backToolStripMenuItem.Text = "Back";
             this.backToolStripMenuItem.Click += new System.EventHandler(this.backToolStripMenuItem_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(175, 631);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Resim Sec";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(30, 159);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(273, 354);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 577);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(175, 555);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 52);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Onay";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 472);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Urun Adi";
+            // 
+            // txtUrunAdi
+            // 
+            this.txtUrunAdi.Location = new System.Drawing.Point(198, 466);
+            this.txtUrunAdi.Name = "txtUrunAdi";
+            this.txtUrunAdi.Size = new System.Drawing.Size(373, 20);
+            this.txtUrunAdi.TabIndex = 7;
+            // 
+            // txtMaliyet
+            // 
+            this.txtMaliyet.Location = new System.Drawing.Point(198, 398);
+            this.txtMaliyet.Name = "txtMaliyet";
+            this.txtMaliyet.Size = new System.Drawing.Size(373, 20);
+            this.txtMaliyet.TabIndex = 9;
+            // 
+            // Maliyet
+            // 
+            this.Maliyet.AutoSize = true;
+            this.Maliyet.Location = new System.Drawing.Point(101, 404);
+            this.Maliyet.Name = "Maliyet";
+            this.Maliyet.Size = new System.Drawing.Size(43, 13);
+            this.Maliyet.TabIndex = 8;
+            this.Maliyet.Text = "Maliyet ";
+            // 
+            // txtYorum
+            // 
+            this.txtYorum.Location = new System.Drawing.Point(198, 332);
+            this.txtYorum.Name = "txtYorum";
+            this.txtYorum.Size = new System.Drawing.Size(373, 20);
+            this.txtYorum.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(101, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Yorum";
+            // 
+            // txtStokMiktari
+            // 
+            this.txtStokMiktari.Enabled = false;
+            this.txtStokMiktari.Location = new System.Drawing.Point(198, 247);
+            this.txtStokMiktari.Name = "txtStokMiktari";
+            this.txtStokMiktari.Size = new System.Drawing.Size(373, 20);
+            this.txtStokMiktari.TabIndex = 13;
+            this.txtStokMiktari.Text = "12";
+            // 
+            // lblStok
+            // 
+            this.lblStok.AutoSize = true;
+            this.lblStok.Location = new System.Drawing.Point(34, 253);
+            this.lblStok.Name = "lblStok";
+            this.lblStok.Size = new System.Drawing.Size(70, 13);
+            this.lblStok.TabIndex = 12;
+            this.lblStok.Text = "Stok MIktari :";
+            // 
+            // txtOzellikler
+            // 
+            this.txtOzellikler.Location = new System.Drawing.Point(198, 180);
+            this.txtOzellikler.Name = "txtOzellikler";
+            this.txtOzellikler.Size = new System.Drawing.Size(373, 20);
+            this.txtOzellikler.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(58, 186);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Ozellikler :";
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(198, 99);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(373, 20);
+            this.txtFiyat.TabIndex = 17;
+            // 
+            // lblFiyat
+            // 
+            this.lblFiyat.AutoSize = true;
+            this.lblFiyat.Location = new System.Drawing.Point(100, 102);
+            this.lblFiyat.Name = "lblFiyat";
+            this.lblFiyat.Size = new System.Drawing.Size(35, 13);
+            this.lblFiyat.TabIndex = 16;
+            this.lblFiyat.Text = "Fiyat :";
+            // 
+            // lblKategori
+            // 
+            this.lblKategori.AutoSize = true;
+            this.lblKategori.Location = new System.Drawing.Point(77, 35);
+            this.lblKategori.Name = "lblKategori";
+            this.lblKategori.Size = new System.Drawing.Size(52, 13);
+            this.lblKategori.TabIndex = 18;
+            this.lblKategori.Text = "Kategori :";
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackgroundImage = global::OnlineTicaretUygulamasi.Properties.Resources.icons8_save_64;
+            this.btnSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSave.Location = new System.Drawing.Point(187, 523);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(40, 40);
+            this.btnSave.TabIndex = 20;
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::OnlineTicaretUygulamasi.Properties.Resources.icons8_exit_67;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(515, 523);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(40, 40);
+            this.button4.TabIndex = 21;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackgroundImage = global::OnlineTicaretUygulamasi.Properties.Resources.icons8_reset_96;
+            this.btnUpdate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnUpdate.Location = new System.Drawing.Point(350, 523);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(40, 40);
+            this.btnUpdate.TabIndex = 22;
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cmbKategori);
+            this.panel1.Controls.Add(this.txtYorum);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtUrunAdi);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.Maliyet);
+            this.panel1.Controls.Add(this.txtMaliyet);
+            this.panel1.Controls.Add(this.lblKategori);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtFiyat);
+            this.panel1.Controls.Add(this.lblStok);
+            this.panel1.Controls.Add(this.lblFiyat);
+            this.panel1.Controls.Add(this.txtStokMiktari);
+            this.panel1.Controls.Add(this.txtOzellikler);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(543, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(858, 607);
+            this.panel1.TabIndex = 23;
+            // 
+            // cmbKategori
+            // 
+            this.cmbKategori.FormattingEnabled = true;
+            this.cmbKategori.Location = new System.Drawing.Point(198, 32);
+            this.cmbKategori.Name = "cmbKategori";
+            this.cmbKategori.Size = new System.Drawing.Size(373, 33);
+            this.cmbKategori.TabIndex = 23;
+            // 
+            // dtUrunler
+            // 
+            this.dtUrunler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtUrunler.Location = new System.Drawing.Point(543, 643);
+            this.dtUrunler.Name = "dtUrunler";
+            this.dtUrunler.RowHeadersWidth = 82;
+            this.dtUrunler.Size = new System.Drawing.Size(861, 226);
+            this.dtUrunler.TabIndex = 24;
+            // 
             // Urunler
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.dtUrunler);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.MaximizeBox = false;
             this.Name = "Urunler";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ürünler";
+            this.Load += new System.EventHandler(this.Urunler_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtUrunler)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,5 +350,29 @@ namespace OnlineTicaretUygulamasi.AnaMenü
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem backToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUrunAdi;
+        private System.Windows.Forms.TextBox txtMaliyet;
+        private System.Windows.Forms.Label Maliyet;
+        private System.Windows.Forms.TextBox txtYorum;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtStokMiktari;
+        private System.Windows.Forms.Label lblStok;
+        private System.Windows.Forms.TextBox txtOzellikler;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Label lblFiyat;
+        private System.Windows.Forms.Label lblKategori;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cmbKategori;
+        private System.Windows.Forms.DataGridView dtUrunler;
     }
 }
--- OnlineTicaretUygulamasi/AnaMenü/Urunler.cs
+++ OnlineTicaretUygulamasi/AnaMenü/Urunler.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using OnlineTicaretUygulamasi.Context;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,16 +14,125 @@ namespace OnlineTicaretUygulamasi.AnaMenü
 {
     public partial class Urunler : Form
     {
+        public string ServerAdress = "11.11.13.8";
+        public string UserName = "sa305";
+        public string Password = "sa305";
+        public string DataBaseName = "Friendyol";
+        public SqlConnection Kopru = new SqlConnection();
+        OpenFileDialog open = new OpenFileDialog();
+
+
         public Urunler()
         {
             InitializeComponent();
         }
+
+        public string Yol { get; private set; }
 
         private void backToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Form1 form1 = new Form1();
             form1.Show();
             this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fn = "";
+            string fl = "";
+            // open file dialog   
+           
+            // image filters  
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp *tif *png )|*.jpg; *.jpeg; *.gif; *.bmp; *.tif; *.png ";
+            // open.Filter = " PDF (*.pdf )| *.pdf)";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                fn = open.SafeFileName.ToString();
+                label1.Text = fn;
+                // display image in picture box  
+                pictureBox1.Image = new Bitmap(open.FileName);
+                // image file path  
+                textBox1.Text = open.FileName;
+                int toplam = textBox1.Text.Length;
+                int dosya = fn.Length;
+                fl = textBox1.Text.Substring(0, toplam - dosya);
+                textBox1.Text = fl;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ResmiTasi(textBox1.Text, label1.Text);
+        }
+
+        private void ResmiTasi(string Yol, string file)
+        {
+            string fileName = file;
+            string sourcePath = @Yol;
+            string targetPath = @"C:\baris\";
+
+            // Use Path class to manipulate file and directory paths.
+            string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
+            string destFile = System.IO.Path.Combine(targetPath, fileName);
+
+            // To copy a folder's contents to a new location:
+            // Create a new target folder.
+            // If the directory
+            System.IO.Directory.CreateDirectory(targetPath);
+            System.IO.File.Copy(sourceFile, destFile,true);
+
+            
+        }
+
+        private void DbSave()
+        {
+
+            string filePath =open.FileName.ToString();
+            string SSorgu= "INSERT INTO [dbo].[Urunler]"+
+          " ([Kategori_ID]"+
+          " ,[Fiyat]" +
+          "  ,[Ozellikler]" +
+          "  ,[StokMiktari]" +
+          "  ,[Yorumlar]" +
+          "  ,[Resimler]" +
+          "  ,[Urun_Maliyeti]" +
+          " ,[UrunAdi])" +
+         " VALUES" +
+          "('" +cmbKategori.SelectedValue+"','"
+          + txtFiyat.Text+"','"
+          + txtOzellikler.Text+"','"
+          + txtStokMiktari.Text+"','"
+          +txtYorum.Text+"','"
+          +filePath+"','"
+          +txtMaliyet.Text+"','"
+          +txtUrunAdi.Text
+          + "')";
+            SSorgu+="";
+            string mesaj = yardimci.Kaydet_Guncelle_Sil(SSorgu, ServerAdress, UserName, Password, DataBaseName);
+            MessageBox.Show(mesaj);
+        }
+
+        private void Urunler_Load(object sender, EventArgs e)
+        {
+            string kSorgu = " SELECT Kategori_ID, Kategori_Name FROM Kategori";
+            Kopru = yardimci.Baglan(ServerAdress, UserName, Password, DataBaseName);
+            DataTable data = new DataTable();
+            data= yardimci.VerileriOku(Kopru, kSorgu);
+            if (data.Rows.Count>0)
+            {
+                DataRow row = data.NewRow();
+                row["Kategori_Name"] = "Lütfen Seçiniz ..";
+                data.Rows.InsertAt(row, 0);
+                cmbKategori.DataSource = data;
+                cmbKategori.DisplayMember = "Kategori_Name";
+                cmbKategori.ValueMember = "Kategori_ID";
+                cmbKategori.SelectedItem = 0;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            DbSave();
         }
     }
 }
